98ed112191fd4ad570062083e230da38ba6e88e4
Elham Hamzapour <ehamzapour@gmail.com>
2022-11-09 12:58:45 -0600

    prompt to choose question from list

--- server.js
+++ server.js
@@ -18,3 +18,53 @@ connection.connect(err => {
     options();
 });
 
+//Prompt to Choose from List
+const options = () => {
+    inquirer.prompt ({
+        type: 'list',
+        name: 'actions',
+        message: 'What would you like to do?',
+        choices: [
+            'View all employees',
+            'View all departments',
+            'View all roles',
+            'Add employee',
+            'Add role',
+            'Add department',
+            'Quit'
+        ]
+
+    })
+
+    .then((answers) => {
+        const { choices } = answers;
+
+        if(choices === "View all employees") {
+            showEmployees();
+        }
+
+        if(choices === "View all departments") {
+            showDepartments();
+        }
+
+        if(choices === "View all roles") {
+            showRole();
+        }
+
+        if(choices === "Add employee") {
+            addEmployee();
+        }
+
+        if(choices === "Add role") {
+            addRole();
+        }
+
+        if(choices === "Add department") {
+            addDepartment();
+        }
+
+        if(choices === "Quit") {
+            connection.end();
+        }
+    })
+};
