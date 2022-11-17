ad9cee2c0b97d428d0e1906133d2c1a2c17677b3
Elham Hamzapour <ehamzapour@gmail.com>
2022-11-17 16:20:01 -0600

    adding functions to view all employees and departments

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const mysql = require ('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
+
 //Connect to Database
 const connection = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: process.env.MYSQL_PASSWORD,
+        password: 'Sardasht1!',
         database: 'employee_db'
     }
 );
@@ -15,22 +16,29 @@ const connection = mysql.createConnection(
 //Connect to MySQL Server and Database
 connection.connect(err => {
     if (err) throw err;
-    options();
+    startPrompt();
 });
+
+//Welcome Message
+startPrompt = () => {
+    console.log("EMPLOYEE TRACKER")
+    options();
+};
 
 //Prompt to Choose from List
 const options = () => {
     inquirer.prompt ({
         type: 'list',
-        name: 'actions',
+        name: 'choices',
         message: 'What would you like to do?',
         choices: [
-            'View all employees',
-            'View all departments',
-            'View all roles',
-            'Add employee',
-            'Add role',
-            'Add department',
+            'View All Employees',
+            'View All Departments',
+            'View All Roles',
+            'Add Employee',
+            'Add Role',
+            'Add Department',
+            'Update Employee Role',
             'Quit'
         ]
 
@@ -48,7 +56,7 @@ const options = () => {
         }
 
         if(choices === "View all roles") {
-            showRole();
+            showRoles();
         }
 
         if(choices === "Add employee") {
@@ -68,3 +76,63 @@ const options = () => {
         }
     })
 };
+
+//View All Employees
+showEmployees = () => {
+    console.log('All Employees...\n');
+
+    const query = `SELECT * FROM employee`;
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log(res.length + ' employees found!');
+        console.table('All Employees:', res);
+        option();
+    })
+                        
+};
+
+//View All Departments
+showDepartments = () => {
+    const sql = 'SELECT * FROM department';
+
+    connection.query((err, res) => {
+        if (err) throw err;
+        console.table('All Departments:', res);
+        option();
+    })
+};
+
+//View All Roles
+showRoles = () => {
+    const sql = 'SELECT * FROM roles';
+
+    connection.query((err, res) => {
+        if (err) throw err;
+        console.table('All Roles:', res);
+        option();
+    })
+};
+
+// //Add Employee
+// addEmployee = () => {
+//     connection.query('SELECT * FROM', (err, res) => {
+//         if (err) throw err;
+//         inquirer.prompt ([
+//             { 
+//                 name: 'first_name',
+//                 type: 'input',
+//                 message: "What is the employee's first name?",
+//             },
+//             {
+//                 name: 'last_name',
+//                 type: 'input',
+//                 message: "What is the employee's last name?",
+//             },
+//             {
+//                 name: 'role_id',
+//                 type: 'input',
+//                 message: "What is the employee's role?",
+//             }
+//         ])
+//     })
